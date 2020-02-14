8b393ad23c4ea9c5719b30b05a00e3b0baac2ec7
terzurumluoglu <34763358+terzurumluoglu@users.noreply.github.com>
2020-02-14 19:53:02 +0300

    edited home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart/cart.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HomeComponent implements OnInit, DoCheck {
+  cart : Product[];
+  isPayment : boolean = false;
+  userName : string = 'User Name';
+  constructor(
+    private _cart : CartService) {
   }
 
+  ngOnInit() {
+    this.cart = this._cart.getCart();
+  }
+
+  ngDoCheck(){
+    this.cart = this._cart.getCart();
+  }
 }
