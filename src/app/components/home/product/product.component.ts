d52b26e3bee7bf6a7f97f7ac11c78023c73719fc
terzurumluoglu <34763358+terzurumluoglu@users.noreply.github.com>
2020-02-14 19:54:49 +0300

    created product component

---
+++ src/app/components/home/product/product.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Product, ProductsList } from "src/app/models/product";
+import { CartService } from 'src/app/services/cart/cart.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+
+export class ProductComponent implements OnInit {
+
+  staRR: number[] = [1, 2, 3, 4, 5];
+  pList: ProductsList;
+  products: Product[] = [];
+  product: Product;
+
+  constructor(private _cart: CartService) {
+    this.pList = new ProductsList();
+    sessionStorage.setItem('cart',JSON.stringify([]));
+  }
+
+  ngOnInit() {
+    this.products = this.pList.getAllProducts();
+  }
+
+  outUpdateData(event: Product) {
+    let i: number = this.products.indexOf(event);
+  }
+
+  lastProductsCount(id: number) {
+    let productCountOnCart: number = this._cart.getCart().filter(p => p.id == id).length;
+    return this.products.find(p => p.id == id).quantity - productCountOnCart
+  }
+
+  add2Cart(p: Product) {
+    if (p != undefined) {
+      if (this.lastProductsCount(p.id) > 0) {
+        this.product = p;
+        let productsOnCart: Product[] = this._cart.getCart();
+        productsOnCart.push(p);
+        this._cart.setCart(productsOnCart);
+        setTimeout(() => {
+          this.product = null;
+        }, 250);
+      }
+    }
+  }
+}
