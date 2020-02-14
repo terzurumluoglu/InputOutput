03809a4a9cc9db9f081ff140f9769c8d8ee93c7b
terzurumluoglu <34763358+terzurumluoglu@users.noreply.github.com>
2020-02-14 19:55:22 +0300

    updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ToastComponent } from './components/home/toast/toast.component';
+import { ProductComponent } from './components/home/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ToastComponent
+    ToastComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
