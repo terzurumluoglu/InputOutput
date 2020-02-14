1dea9025d2cb3fb4dd640f69ec8ca836aa2c6c4d
terzurumluoglu <34763358+terzurumluoglu@users.noreply.github.com>
2020-02-14 19:55:33 +0300

    updated app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotfoundComponent } from './components/home/notfound/notfound.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./components/home/home.module')
+      .then(m => m.HomeModule)
+  },
+  {
+    path: '**',
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
